f184d603b69eb29011b41df8fbd2bce01bc42163
Matt Loar <matt@loar.name>
2016-09-24 22:09:14 -0500

    Support Lecture post type

--- loop-print.php
+++ loop-print.php
@@ -1,4 +1,7 @@
-<?php if ( have_posts() ): ?>
+<?php 
+  global $query_string;
+  query_posts($query_string . '&meta_key=ilr_post_type&meta_value=article&posts_per_page=-1');
+  if ( have_posts() ): ?>
 
   <!-- SECTION: Articles -->
   <span class="label is-primary">Articles</span>
@@ -11,7 +14,40 @@
       $subtitle = get_field('ilr_subtitle');
       $author = get_field('ilr_author');
 
-      if ( $label === 'Article' ):
+      ?>
+
+        <li class="item<?php if ( !empty($author) ): ?> has-author<?php endif; ?>">
+
+          <h2 class="title"><a href="<?php the_permalink(); ?>"><?=$title?>&nbsp;</a></h2>
+          <!-- leave the non-breaking space; it's a hack -->
+
+          <?php if ( !empty($author) ) : ?>
+            <span class="post-meta">
+              <span class="author"><?=$author?></span>
+            </span>
+          <?php endif; ?>
+
+        </li>
+
+    <?php endwhile; ?>
+
+  </ul>
+
+<?php
+  endif;
+  query_posts($query_string . '&meta_key=ilr_post_type&meta_value=lecture&posts_per_page=-1');
+  if ( have_posts() ): ?>
+
+  <!-- SECTION: Notes -->
+  <span class="label is-primary">David C. Baum Memorial Lecture</span>
+  <ul class="group">
+
+    <?php while ( have_posts() ) : the_post();
+
+      $label = ucfirst( get_field('ilr_post_type') );
+      $title = get_the_title();
+      $subtitle = get_field('ilr_subtitle');
+      $author = get_field('ilr_author');
 
       ?>
 
@@ -28,13 +64,14 @@
 
         </li>
 
-      <?php endif; ?>
-
     <?php endwhile; ?>
 
   </ul>
 
-  <?php rewind_posts(); ?>
+<?php
+  endif;
+  query_posts($query_string . '&meta_key=ilr_post_type&meta_value=note&posts_per_page=-1');
+  if ( have_posts() ): ?>
 
   <!-- SECTION: Notes -->
   <span class="label is-primary">Notes</span>
@@ -47,8 +84,6 @@
       $subtitle = get_field('ilr_subtitle');
       $author = get_field('ilr_author');
 
-      if ( $label === 'Note' ):
-
       ?>
 
         <li class="item<?php if ( !empty($author) ): ?> has-author<?php endif; ?>">
@@ -64,19 +99,8 @@
 
         </li>
 
-      <?php endif; ?>
-
     <?php endwhile; ?>
 
   </ul>
 
 <?php endif; ?>
-
-
-
-
-
-
-
-
-
