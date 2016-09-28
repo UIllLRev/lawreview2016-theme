c0d006c0c5c48499ed5f6b56408e925d63a9eb0c
Matt Loar <matt@loar.name>
2016-09-27 19:50:04 -0500

    Essays AND Book Review Essays

--- loop-print.php
+++ loop-print.php
@@ -108,6 +108,41 @@
   query_posts($query_string . '&meta_key=ilr_post_type&meta_value=essay&posts_per_page=-1&orderby=date&order=ASC');
   if ( have_posts() ): ?>
 
+  <!-- SECTION: Essays -->
+  <span class="label is-primary">Essay<?php if (lawreview_post_count() > 1) echo 's'; ?></span>
+  <ul class="group">
+
+    <?php while ( have_posts() ) : the_post();
+
+      $label = ucfirst( get_field('ilr_post_type') );
+      $title = get_the_title();
+      $subtitle = get_field('ilr_subtitle');
+      $author = get_field('ilr_author');
+
+      ?>
+
+        <li class="item<?php if ( !empty($author) ): ?> has-author<?php endif; ?>">
+
+          <h2 class="title"><a href="<?php the_permalink(); ?>"><?=$title?>&nbsp;</a></h2>
+          <!-- leave the non-breaking space; it's a hack -->
+
+          <?php if ( !empty($author) ) : ?>
+            <span class="post-meta">
+              <span class="author"><?=$author?></span>
+            </span>
+          <?php endif; ?>
+
+        </li>
+
+    <?php endwhile; ?>
+
+  </ul>
+
+<?php
+  endif;
+  query_posts($query_string . '&meta_key=ilr_post_type&meta_value=review&posts_per_page=-1&orderby=date&order=ASC');
+  if ( have_posts() ): ?>
+
   <!-- SECTION: Book Review Essays -->
   <span class="label is-primary">Book Review Essay<?php if (lawreview_post_count() > 1) echo 's'; ?></span>
   <ul class="group">
