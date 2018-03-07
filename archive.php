f18f072e07398d75ebd103ac1b28ad5251ee38d0
Bradley Williams <bcwilliams28@gmail.com>
2018-03-07 08:46:58 -0600

    Initial Test for New Collections Section
    First iterations of converting 'First 100 Days' template into single.php
    for the 'ilr_symposium" post type

--- archive.php
+++ archive.php
@@ -1,19 +1,49 @@
 <?php get_header(); ?>
 
 	<main role="main">
-		<!-- section -->
-		<section>
 
-			<h1><?php _e( 'Archives', 'lawreview' ); ?></h1>
+    <!-- banner -->
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
 
-			<?php get_template_part('loop'); ?>
+          <h1 class="title is-page-title">
+            <?php
+              $postType = get_queried_object();
+              echo esc_html($postType->labels->name); ?>
+          </h1>
+        </div>
+      </div>
+    </section>
 
-			<?php get_template_part('pagination'); ?>
+    <!-- inner navigation -->
+    <?php if ( $postType->labels->name === 'Symposiums'): ?>
+        <nav class="nav nav-secondary has-shadow">
+            <div class="container">
+              <div class="nav-center">
 
-		</section>
-		<!-- /section -->
+                <a href="#symposiums" class="nav-item is-tab">Symposiums</a><a href="#response" class="nav-item is-tab">Responses</a>
+
+              </div>
+            </div>
+        </nav>
+    <?php endif; ?>
+
+    <!-- events -->
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-10 is-offset-1 main-content">
+
+            <?php get_template_part('loop', 'collections'); ?>
+            <?php get_template_part('pagination'); ?>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
---
+++ loop-collections.php
@@ -0,0 +1,64 @@
+<?php
+
+$post_types = lawreview_get_post_types();
+
+$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
+query_posts( array(
+  'post_type' => 'ilr_symposium',
+  'posts_per_page' => -1,
+  'post_parent' => 0,
+  'paged' => $paged,
+  'orderby' => 'date',
+  'order' => 'DESC',
+));
+
+if ( have_posts() ) : ?>
+
+  <?php while ( have_posts() ) : the_post();
+
+    $label = $post_types[get_field('ilr_post_type')];
+    $subtitle = get_field('ilr_subtitle');
+    // like strip_tags() only removes content too
+    // we do this to remove star footnote
+    $author = preg_replace('@<(\w+)\b.*?>.*?</\1>@si', '', get_field('ilr_author'));
+    $citation = get_field('ilr_citation');
+
+    ?>
+
+    <article id="post-<?php the_ID(); ?>" class="post">
+
+      <?php if ( !empty($label) ) : ?>
+        <span class="tag is-primary"><?=$label?></span>
+      <?php endif; ?>
+
+      <h2 class="title is-3"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+      <?php if ( !empty($subtitle) ) : ?>
+        <h3 class="subtitle is-4"><?=$subtitle?></h3>
+      <?php endif; ?>
+
+      <?php if ( !empty($author) || !empty($citation) ) : ?>
+        <div class="post-meta">
+          <?php if ( !empty($author) ) : ?>
+            <span class="author">by <?=$author?></span>
+          <?php endif; ?>
+          <?php if ( !empty($citation) ) : ?>
+            <span class="citation"><?=$citation?></span>
+          <?php endif; ?>
+        </div>
+        <?php endif; ?>
+
+    </article>
+
+  <?php endwhile; ?>
+
+<?php else : ?>
+
+  <article class="post">
+    <h2 class="title is-3">Nothing to display</h2>
+    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+  </article>
+
+<?php endif; ?>
+
