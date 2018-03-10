ecaea19a1b1c9d3490fb7f59455589e3f60f9dcf
Chris Lusk <chris.m.lusk@gmail.com>
2018-03-10 14:23:11 -0600

    update template to fit theme

--- archive.php
+++ archive.php
@@ -1,19 +1,28 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
-
-			<h1><?php _e( 'Archives', 'lawreview' ); ?></h1>
-
-			<?php get_template_part('loop'); ?>
-
-			<?php get_template_part('pagination'); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+  <main class="main">
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <?php
+            $post_type = get_queried_object();
+            echo '<h1 class="title is-page-title">' . esc_html($post_type->labels->name) . '</h1>';
+          ?>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <?php
+            echo '<div class="column is-10 is-offset-1 main-content">';
+                    get_template_part('loop');
+                    get_template_part('pagination');
+            echo '</div>';
+          ?>
+        </div>
+      </div>
+    </section>
+  </main>
 
 <?php get_footer(); ?>
